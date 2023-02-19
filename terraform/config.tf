6f2e565489e1dfb92707a4fbabe3d8a1e87732a2
Duy Hiếu <hidxhidxhid@gmail.com>
2023-02-19 11:11:38 +0700

    change strucutire

--- terraform/config.tf
+++ terraform/config.tf
@@ -10,11 +10,3 @@ terraform {
 provider "digitalocean" {
     token = var.do_token
 }
-
-resource "digitalocean_droplet" "kafka" {
-  image  = "ubuntu-20-04-x64"
-  name   = "kafka"
-  region = "sgp1"
-  size   = "s-4vcpu-8gb"
-  ssh_keys = [var.ssh_key_id]
-}
---
+++ terraform/server.tf
@@ -0,0 +1,7 @@
+resource "digitalocean_droplet" "kafka" {
+  image  = "ubuntu-20-04-x64"
+  name   = "kafka"
+  region = "sgp1"
+  size   = "s-4vcpu-8gb"
+  ssh_keys = [var.ssh_key_id]
+}
